89712567d2d5ece16c06278aeb5046513b680777
chaitany310 <chaitany31001@gmail.com>
2023-06-09 17:59:27 +0530

    my weather app

---
+++ weatherapp/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from  .import views
+urlpatterns = [
+    path('', views.SignupPage, name='signup'),
+    path('login/', views.LoginPage, name='login'),
+    path('home/', views.index, name='home'),
+    path('logout/', views.LogoutPage, name='logout'),
+
+]
---
+++ weatherapp/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import urllib.request
+import json
+
+# Create your views here.
+@login_required(login_url='login')
+
+def index(request):
+
+    if request.method == 'POST':
+        city = request.POST['city']
+
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
+                                        city + '&units=metric&appid=b7e5796203b13d11f8b5633de0896ccb').read()
+        list_of_data = json.loads(source)
+
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ', '
+            + str(list_of_data['coord']['lat']),
+
+            "temp": str(list_of_data['main']['temp']) + ' °C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            'main': str(list_of_data['weather'][0]['main']),
+            'description': str(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon'],
+        }
+        print(data)
+    else:
+        data = {}
+
+    return render(request, "index.html", data)
+
+
+def SignupPage(request):
+    if request.method == 'POST':
+        uname = request.POST.get('username')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('password1')
+        pass2 = request.POST.get('password2')
+
+        if pass1 != pass2:
+            return HttpResponse("Your password and confrom password are not Same!!")
+        else:
+
+            my_user = User.objects.create_user(uname, email, pass1)
+            my_user.save()
+            return redirect('login')
+
+    return render(request, 'signup.html')
+
+
+def LoginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass')
+        user = authenticate(request, username=username, password=pass1)
+        if user is not None:
+            login(request, user)
+            data = {user: user}
+            return redirect('home')
+        else:
+            return HttpResponse("Username or Password is incorrect!!!")
+
+    return render(request, 'login.html')
+
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
